2b31e1ae2fe30a1157c8683a9a66e1572d7bc3ca
touseef138 <touseef@pixarsart.com>
2023-11-21 18:30:23 +0500

    setup fonts done

---
+++ src/theme/fontFamily.js
@@ -0,0 +1,7 @@
+export const fontFamily = {
+  regular: 'Nunito-Regular',//400
+  medium: 'Nunito-Medium',//500
+  semiBold: 'Nunito-SemiBold',//600
+  bold:"Nunito-Bold",//700
+  extraBold:"Nunito-ExtraBold"//800
+};
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+// import {colors} from './colors';
+// import {window} from './window';
+import { fontFamily } from "./fontFamily";
+
+export default theme = {
+  // colors,
+  // window,
+  fontFamily,
+};
